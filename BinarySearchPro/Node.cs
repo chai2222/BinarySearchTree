c4725510bd68ef8270834da0fd33d23db401f39d
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-19 21:38:59 +0530

    LeftRightNode

---
+++ BinarySearchPro/Node.cs
@@ -0,0 +1,118 @@
+﻿
+namespace BinarySearchPro
+{
+    //Generic class where T is IComparable
+    public class BinarySearch<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+
+
+        //creating get set method for lefttree and righttree
+        public BinarySearch<T> leftTree { get; set; }
+        public BinarySearch<T> rightTree { get; set; }
+
+        public BinarySearch(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.rightTree = null;
+            this.leftTree = null;
+
+        }
+
+        //defining static variables so that we can get proper count 
+        public static int leftCount = 0, rightCount = 0;
+
+        //this variable created to get result of search method
+        //public bool result;
+
+
+        //Creating Add method where parameter as T 
+
+        public void Insert(T item)
+        {
+            T root = this.NodeData;
+
+            if (root.CompareTo(item) > 0)
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new BinarySearch<T>(item);
+
+                }
+                else
+                    this.leftTree.Insert(item);
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new BinarySearch<T>(item);
+                }
+                else
+                    this.rightTree.Insert(item);
+            }
+        }
+
+        //Display method to add elements in respective position
+
+        public void Display()
+        {
+            Console.WriteLine(this.NodeData.ToString());
+
+            if (this.leftTree != null)
+            {
+                leftCount++;
+                Console.Write("On Left Tree :");
+                this.leftTree.Display();
+            }
+
+            if (this.rightTree != null)
+            {
+                rightCount++;
+                Console.Write("On Right Tree :");
+                this.rightTree.Display();
+            }
+        }
+        //Size method shows element present in the tree
+
+        public void Getsize()
+        {
+            Console.WriteLine("Size of the tree is :" + " " + (1 + leftCount + rightCount));
+        }
+
+        //Search method to search for a particular element 
+
+        public bool Search(T element, BinarySearch<T> node)
+        {
+            if (node == null)
+                return false;
+
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Element Found : " + node.NodeData);
+                return true;
+            }
+            //else
+            //{
+            //    Console.WriteLine("Element Not Found : ");
+
+            //}
+
+            //Now checking to add element in respective Side
+            //
+            if (element.CompareTo(node.NodeData) < 0)
+            {
+                Search(element, node.leftTree);
+            }
+            if (element.CompareTo(node.NodeData) > 0)
+            {
+                Search(element, node.rightTree);
+
+            }
+
+            return true;
+
+        }
+
+    }
+}
--- BinarySearchPro/Program.cs
+++ BinarySearchPro/Program.cs
@@ -1,1 +1,90 @@
-﻿//Welcome to Binary Search Tree Program
+﻿namespace BinarySearchPro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program");
+
+            bool check = true;
+
+
+            Console.WriteLine("Select Options :\n" +
+                "1)Enter elements in the Binary Search Tree\n" +
+                "2)Added Elements from the Figure and Get the size\n" +
+                "3)Searching Particular Element from the Binary Search Tree\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    BinarySearch<int> binary = new BinarySearch<int>(56);        //56 taken as a root element
+                    binary.Insert(30);
+                    binary.Insert(70);
+                    Console.WriteLine("::::::::::::::::::::::::::::::::::");
+                    Console.Write("Root Node is : ");
+                    binary.Display();
+                    break;
+
+                case 2:
+
+                    BinarySearch<int> binary1 = new BinarySearch<int>(56);        //56 taken as a root element
+                    binary1.Insert(30);
+                    binary1.Insert(70);
+                    binary1.Insert(22);
+                    binary1.Insert(40);
+                    binary1.Insert(60);
+                    binary1.Insert(95);
+                    binary1.Insert(11);
+                    binary1.Insert(65);
+                    binary1.Insert(3);
+                    binary1.Insert(16);
+                    binary1.Insert(63);
+                    binary1.Insert(67);
+                    Console.WriteLine("::::::::::::::::::::::::::::::::::");
+                    Console.Write("Root Node is : ");
+                    binary1.Display();
+                    Console.WriteLine("::::::::::::::::::::::::::::::::::");
+
+                    binary1.Getsize();
+                    Console.WriteLine("::::::::::::::::::::::::::::::::::");
+
+                    break;
+
+                case 3:
+                    BinarySearch<int> binary2 = new BinarySearch<int>(56);
+
+                    binary2.Insert(30);
+                    binary2.Insert(70);
+                    binary2.Insert(22);
+                    binary2.Insert(40);
+                    binary2.Insert(60);
+                    binary2.Insert(95);
+                    binary2.Insert(11);
+                    binary2.Insert(65);
+                    binary2.Insert(3);
+                    binary2.Insert(16);
+                    binary2.Insert(63);
+                    binary2.Insert(67);
+                    Console.WriteLine("::::::::::::::::::::::::::::::::::");
+                    Console.Write("Root Node is : ");
+                    binary2.Display();
+                    Console.WriteLine("::::::::::::::::::::::::::::::::::");
+                    binary2.Getsize();
+                    Console.WriteLine(binary2.Search(2, binary2));
+
+
+
+                    Console.WriteLine("::::::::::::::::::::::::::::::::::");
+                    break;
+
+                default:
+
+                    Console.WriteLine("Please enter Proper option");
+                    break;
+            }
+        }
+    }
+}
